3b273bd3dfbdbf44cb49b29f83aac84579604814
Huyghues des Etages Thomas <10020@odhlm93.pri>
2019-06-07 10:49:57 +0200

    few changes

--- TP01/routes/web.php
+++ TP01/routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', function () {
         'name' => 'Damage Germany',
         'description' => 'Tekken matches',
         'location' => 'Germany',
-        'price' => '20'
+        'prix' => '20'
         ]);
 
     $events = App\Event::all();
