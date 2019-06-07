1183e2d377844a05e5ac78ab2e40d39083c5724b
Huyghues des Etages Thomas <10020@odhlm93.pri>
2019-06-07 15:24:51 +0200

    few changes

--- TP01/app/helpers.php
+++ TP01/app/helpers.php
@@ -3,10 +3,10 @@
 
 if(!function_exists('format_price')){
     function format_price($event){
-        if($event-price == 0){
+        if($event->prince == 0){
             return '<strong>Gratuit</strong>';
         } else {
-            return $event->price . 'euros';
+            return $event->prince . 'euros';
         }
     }
 }
