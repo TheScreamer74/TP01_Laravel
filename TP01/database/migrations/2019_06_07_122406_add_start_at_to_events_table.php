a1926c408b461d9b3c99aadf3d1904e1901adc5e
Huyghues des Etages Thomas <10020@odhlm93.pri>
2019-06-07 14:28:28 +0200

    few changes

--- TP01/database/migrations/2019_06_07_122406_add_start_at_to_events_table.php
+++ TP01/database/migrations/2019_06_07_122406_add_start_at_to_events_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddStartAtToEventTable extends Migration
+class AddStartAtToEventsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class AddStartAtToEventTable extends Migration
      */
     public function up()
     {
-        Schema::table('event', function (Blueprint $table) {
+        Schema::table('events', function (Blueprint $table) {
             $table->dateTime('starts_at');
         });
     }
@@ -25,7 +25,7 @@ class AddStartAtToEventTable extends Migration
      */
     public function down()
     {
-        Schema::table('event', function (Blueprint $table) {
+        Schema::table('events', function (Blueprint $table) {
             $table->dropColum('start_at');
         });
     }
