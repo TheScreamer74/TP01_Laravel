3c7569a67e6719f4757ac369e2d2cc834a195c75
Huyghues des Etages Thomas <10020@odhlm93.pri>
2019-06-11 11:39:42 +0200

    debut du tp

--- TP02-Raccourcisseur_d_url/app/Url.php
+++ TP02-Raccourcisseur_d_url/app/Url.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Url extends Model
 {
-    public $timestamps = false;
+    protected $timestamps = false;
+    protected $fillable = ['url', 'shortened'];
 }
