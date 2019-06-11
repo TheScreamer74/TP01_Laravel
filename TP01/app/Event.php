d3892a8d2efa170d314f8242193c4644492a591e
Huyghues des Etages Thomas <10020@odhlm93.pri>
2019-06-11 09:31:34 +0200

    few changes

--- TP01/app/Event.php
+++ TP01/app/Event.php
@@ -8,7 +8,10 @@ class Event extends Model
 {
     protected $fillable = ['name', 'description', 'location', 'prince', 'starts_at'];
 
-    protected $dates = ['starts_at'];
+    protected $casts = [
+      'starts_at' => 'datetime',
+        'prince' => 'float'
+    ];
 
     public function isFree(){
         return $this->prince == 0;
