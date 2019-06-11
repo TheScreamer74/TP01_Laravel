ade62b55942883b74aed87df9b97cefa935ef753
Huyghues des Etages Thomas <10020@odhlm93.pri>
2019-06-11 15:58:36 +0200

    debut du tp

--- TP02-Raccourcisseur_d_url/resources/views/welcome.blade.php
+++ TP02-Raccourcisseur_d_url/resources/views/welcome.blade.php
@@ -3,9 +3,13 @@
 @section('content')
     <h1>raccourcisseur d'URL</h1>
 
+
+
+
     <form action="" method="POST">
         {{ csrf_field() }}
         <input type="text" name="url" placeholder="Entrez votre url ici">
+        <p>{{ $errors->first('url') }}</p>
         <input type="submit" value="Shorten URL">
     </form>
 @stop
