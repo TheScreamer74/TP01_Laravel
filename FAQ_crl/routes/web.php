1d3d63105088ef26ab07c539d4c02ad57f1753c2
TheScreamer74 <blakilleur@gmail.com>
2019-06-14 13:25:40 +0000

    Big commit

--- FAQ_crl/routes/web.php
+++ FAQ_crl/routes/web.php
@@ -11,7 +11,19 @@
 |
 */
 
-Route::get('/', 'CategoryController@index')->name('category.index');
+
+
+/*
+|--------------------------------------------------------------------------
+| /ask + <parameters>
+|--------------------------------------------------------------------------
+|
+| The routes to access to QuestionController
+| You can create, Modify and Delete entries
+|
+|
+|
+*/
 
 Route::get('/ask', 'QuestionController@create')->name('question.create');
 
@@ -23,15 +35,55 @@ Route::post('/ask/{id}', 'QuestionController@update')->name('question.update');
 
 Route::delete('ask/{id}', 'QuestionController@destroy')->name('question.destroy');
 
+/*
+|--------------------------------------------------------------------------
+| /redact + <parameters>
+|--------------------------------------------------------------------------
+|
+| The routes to access to CategoryController
+| You can create, Modify and Delete entries
+|
+|
+|
+*/
+
 Route::get('/redact', 'CategoryController@create')->name('category.create');
 
 Route::post('/redact', 'CategoryController@store')->name('category.store');
+
+Route::put('/redact/{id}', 'CategoryController@edit')->name('category.edit');
+
+Route::post('/ask/{id}', 'CategoryController@update')->name('category.update');
+
+Route::delete('/redact/{id}', 'CategoryController@destroy')->name('category.destroy');
+
+/*
+|--------------------------------------------------------------------------
+| /contact
+|--------------------------------------------------------------------------
+|
+| The routes to access to ContactController
+| There's no entries in the database, this controller send mails
+|
+|
+|
+*/
 
 Route::get('/contact', 'ContactController@index')->name('contact.index');
 
 Route::post('/contact', 'ContactController@store')->name('contact.store');
 
-Route::put('/{id}', 'CategoryController@edit')->name('category.edit');
+/*
+|--------------------------------------------------------------------------
+| Root Route
+|--------------------------------------------------------------------------
+|
+| 
+|
+|
+|
+|
+*/
 
-Route::delete('/{id}', 'CategoryController@destroy')->name('category.destroy');
+Route::get('/', 'CategoryController@index')->name('category.index');
 
